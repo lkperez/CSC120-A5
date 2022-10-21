50a4d7da779bda7bd54e3e2eb5f79f1a7f7c8c70
lkperez <112982320+lkperez@users.noreply.github.com>
2022-10-21 15:24:36 -0400

    Update Shirt.java

--- Shirt.java
+++ Shirt.java
@@ -4,13 +4,26 @@
  * @author R. Jordan Crouser + CSC120 (Fall '22))
  * @version 13 October 2022
  */
+
+  /**
+     * Making shirt for the scarecrow
+     * @param none
+     * @return display of shirt
+     */
 public class Shirt {
 
     /*
      * TODO: Modify this method to print ASCII Art Shirt
      */
     public void display() {
-        System.out.println("Shirt?);
+        System.out.println("Shirt?");
+        System.out.println("                   __,--./|.--,__");
+        System.out.println("                .`   \\ \\ / /    `.");
+        System.out.println("               .`      \\ | /       `.");
+        System.out.println("              /   /     ^|^      \\   \\");
+        System.out.println("             /   / |     |o     | \\   \\");
+        System.out.println("            /===/  |     |      |  \\===\\");
+        System.out.println("           /___/   |     |o     |   \\___\\");
     }
 
     /* main method (for testing) */
