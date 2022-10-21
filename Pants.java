836d784e777d17cdf2de231539d831c61beac46c
lkperez <112982320+lkperez@users.noreply.github.com>
2022-10-21 15:20:24 -0400

    Update Pants.java

--- Pants.java
+++ Pants.java
@@ -4,13 +4,29 @@
  * @author R. Jordan Crouser + CSC120 (Fall '22))
  * @version 13 October 2022
  */
+
+ /**
+     * Making pants for the scarecrow
+     * @param none
+     * @return display of pants
+     */
 public class Pants {
 
     /*
      * TODO: Modify this method to print ASCII Art Pants
      */
     public void display() {
-        System.out.println("Pants?);
+        System.out.println("Pants?");
+        System.out.println("              |___/\\___|");
+        System.out.println("              | | \\/ | |");
+        System.out.println("              |_/ || \\_|");
+        System.out.println("              |   ||   |");
+        System.out.println("              |   ||   |");
+        System.out.println("              |   ||   |");
+        System.out.println("              |   ||   |");
+        System.out.println("              |   ||   |");
+        System.out.println("              |___||___|");
+        
     }
 
     /* main method (for testing) */
