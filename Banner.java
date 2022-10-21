f38afdf0a9d7ffdf1a3504b87bb7f7ee62f8c66a
lkperez <112982320+lkperez@users.noreply.github.com>
2022-10-21 15:15:52 -0400

    Update Banner.java

--- Banner.java
+++ Banner.java
@@ -4,6 +4,12 @@
  * @author R. Jordan Crouser + CSC120 (Fall '22))
  * @version 13 October 2022
  */
+
+  /**
+     * Making a banner for the message passed
+     * @param message
+     * @return display of banner with message
+     */
 public class Banner {
     
     /* Message to print on Banner */
@@ -18,7 +24,13 @@ public class Banner {
      * TODO: Modify this method to print a decorative banner, resized to fit the message
      */
     public void display() {
+        System.out.println("~***********~");
+        System.out.println("|************|");
+        System.out.println("|            |");
         System.out.println(this.message);
+        System.out.println("|            |");
+        System.out.println("|************|");
+        System.out.println("~************~");
     }
 
     /* main method (for testing) */
